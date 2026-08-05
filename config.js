f1925ac4162042053acba9ef217d6999d8d4d2f2
Ignacio <sistemas@raitrai.cl>
2026-08-05 17:11:23 -0400

    Update and rename config,js to config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const PORTAL_CONFIG = Object.freeze({
+  apiUrl:
+    "https://consultarpulserapublica-r3llfis4wa-tl.a.run.app",
+
+  sessionTokenKey:
+    "raitrai_nfc_session_token_v2",
+
+  selectedGroupKey:
+    "raitrai_nfc_selected_group_v2"
+});
